2bcdd7e5289ae8b1d213b04da9a8c828b5e49931
StillOP <pierrebetambo@yahoo.fr>
2018-02-15 23:50:34 +0100

    v 1.1

--- Site e-commerce/audio.player.php
+++
@@ -1,8 +0,0 @@
-<script>
-    document.querySelector('#album-play').addEventListener('click', function() {
-        
-        document.querySelector('#playlist').innerHTML += '<audio controls="controls" id="player"></audio>';
-        var player = document.query
-        
-    });
-</script>
